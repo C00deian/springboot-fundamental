af3d11e1f7daa931db01e60f12ab7f5f01ac7b0c
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-15 01:50:48 +0530

    removed unused code

--- src/main/java/com/notification/demo/DemoApplication.java
+++ src/main/java/com/notification/demo/DemoApplication.java
@@ -1,10 +1,5 @@
 package com.notification.demo;
 
-import com.notification.demo.entities.Address;
-import com.notification.demo.entities.Profile;
-import com.notification.demo.entities.Tag;
-import com.notification.demo.entities.User;
-import lombok.Builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,20 +11,6 @@ public class DemoApplication {
 
 	SpringApplication.run(DemoApplication.class, args);
 
-	var user = User.builder()
-		.name("John")
-		.password("password")
-		.email("john9@gmail.com")
-		.build();
-
-
-	var profile = Profile.builder()
-			.bio("Biography")
-			.build();
-
-	user.setProfile(profile);
-	profile.setUser(user);
-		System.out.println(user);
 
 	}
 
