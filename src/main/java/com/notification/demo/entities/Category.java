56d1802b9ceb11ef5e5190418e22ca9f08ce56f4
ritik kumar <ritikkumarkashyap9@gmail.com>
2025-05-15 00:42:49 +0530

    defining two entity product and category and also set relationship

---
+++ src/main/java/com/notification/demo/entities/Category.java
@@ -0,0 +1,29 @@
+package com.notification.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Short id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products = new HashSet<>();
+}
+
---
+++ src/main/java/com/notification/demo/entities/Product.java
@@ -0,0 +1,29 @@
+package com.notification.demo.entities;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
+
